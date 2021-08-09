d87a09145b7b896ca663b25536ed27a70c08d1b0
air <vip.vska.vip11@gmail.com>
2021-08-09 23:34:45 +0300

    Create ContactUs model in currency app

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=50)
+    subject = models.CharField(max_length=250)
+    message = models.CharField(max_length=2050)
