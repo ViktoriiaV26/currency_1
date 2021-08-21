a8009ec4727ed7d09c75548c6def49cabb80d275
air <vip.vska.vip11@gmail.com>
2021-08-21 22:02:48 +0300

    Created ContactUs object and add  function 'contactus_list'

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from currency.views import hello_world
+from currency.views import hello_world, contactus_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # currency
+    path('contactus/list/', contactus_list),
     path('hello-world/', hello_world),
 ]
