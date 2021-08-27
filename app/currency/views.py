1474f5592ef2e47223b98c59919711419a248364
air <vip.vska.vip11@gmail.com>
2021-08-27 16:42:07 +0300

    Create base.html and showed all objects rate and contactus in the form of the table

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,17 +1,28 @@
 from django.http import HttpResponse
-from currency.models import ContactUs
+from django.shortcuts import render
+
+from currency.models import ContactUs, Rate
 
 
 def hello_world(request):
     return HttpResponse('Hello World')
 
 
+def index(request):
+    return render(request, 'index.html')
+
+
 def contactus_list(request):
     contact = ContactUs.objects.all()
-    result = []
-    for c in contact:
-        result.append(
-            f'Id: {c.id} Email: {c.email_from} Subject: {c.subject} Message: {c.message}</br>'
-        )
+    context = {
+        'contactus_list': contact,
+    }
+    return render(request, 'contactus_list.html', context=context)
 
-    return HttpResponse(str(result))
+
+def rate_list(request):
+    rates = Rate.objects.all()
+    context = {
+        'rate_list': rates,
+    }
+    return render(request, 'rate_list.html', context=context)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from currency.views import hello_world, contactus_list
+from currency.views import index, hello_world, contactus_list, rate_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
 
     # currency
+    path('rate/list/', rate_list),
     path('contactus/list/', contactus_list),
     path('hello-world/', hello_world),
 ]
