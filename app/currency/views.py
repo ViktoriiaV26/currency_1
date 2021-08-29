3eb3911b68b098d8513c195bbf718d5d858fbe96
air <vip.vska.vip11@gmail.com>
2021-08-29 16:04:17 +0300

    Created model Source and forms /create/update/.for it. Showed details and added a delete object Source.

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
 
-from currency.models import ContactUs, Rate
+from currency.models import ContactUs, Rate, Source
+from currency.forms import SourceForm
 
 
 def hello_world(request):
@@ -26,3 +27,65 @@ def rate_list(request):
         'rate_list': rates,
     }
     return render(request, 'rate_list.html', context=context)
+
+
+def source_list(request):
+    source = Source.objects.all()
+    context = {
+        'source_list': source,
+    }
+    return render(request, 'source_list.html', context=context)
+
+
+def source_create(request):
+    if request.method == 'POST':
+        form = SourceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/source/list/')
+    elif request.method == 'GET':
+        form = SourceForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'source_create.html', context=context)
+
+
+def source_details(request, source_id):
+    # try:
+    #     source = Source.objects.get(id=source_id)
+    # except Source.DoesNotExist as exc:
+    #     raise Http404(exc)
+    source = get_object_or_404(Source, id=source_id)
+
+    context = {
+        'object': source,
+    }
+    return render(request, 'source_details.html', context=context)
+
+
+def source_update(request, source_id):
+    source = get_object_or_404(Source, id=source_id)
+    if request.method == 'POST':
+        form = SourceForm(request.POST, instance=source)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/source/list/')
+    elif request.method == 'GET':
+        form = SourceForm(instance=source)
+    context = {
+        'form': form,
+    }
+    return render(request, 'source_update.html', context=context)
+
+
+def source_delete(request, source_id):
+    source = get_object_or_404(Source, id=source_id)
+    if request.method == 'POST':
+        source.delete()
+        return HttpResponseRedirect('/source/list/')
+
+    context = {
+        'object': source,
+    }
+    return render(request, 'source_delete.html', context=context)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from currency.views import index, hello_world, contactus_list, rate_list
+from currency.views import (index, hello_world,
+                            contactus_list, rate_list,
+                            source_create, source_list, source_details,
+                            source_update, source_delete
+                            )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,6 +12,11 @@ urlpatterns = [
 
     # currency
     path('rate/list/', rate_list),
+    path('source/list/', source_list),
+    path('source/create/', source_create),
+    path('source/details/<int:source_id>/', source_details),
+    path('source/update/<int:source_id>/', source_update),
+    path('source/delete/<int:source_id>/', source_delete),
     path('contactus/list/', contactus_list),
     path('hello-world/', hello_world),
 ]
