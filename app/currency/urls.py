9a11ee91cddc37440276de212dd211dbf74d5a88
air <vip.vska.vip11@gmail.com>
2021-09-02 13:01:09 +0300

    Created CRUD operations for the model Source based on class base views and module urls.py in aplication currency. Function reverse for all links in python code.

---
+++ app/currency/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from currency.views import (
+                  RateListView, ContactusListView,
+                  SourceListView, SourceCreateView, SourceDetailView, SourceUpdateView, SourceDeleteView,)
+
+
+app_name = 'currency'
+
+urlpatterns = [
+    path('rate/list/', RateListView.as_view(), name='rate-list'),
+    path('source/list/', SourceListView.as_view(), name='source-list'),
+    path('source/create/', SourceCreateView.as_view(), name='source-create'),
+    path('source/details/<int:pk>/', SourceDetailView.as_view(), name='source-details'),
+    path('source/update/<int:pk>/', SourceUpdateView.as_view(), name='source-update'),
+    path('source/delete/<int:pk>/', SourceDeleteView.as_view(), name='source-delete'),
+    path('contactus/list/', ContactusListView.as_view(), name='contactus-list'),
+
+]
